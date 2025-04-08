b6e914a070fcf87f7170097d5a372dd249ce7c67
kevmoslin <kevinsmostacero@gmail.com>
2025-04-08 16:43:37 -0500

    ARREGLANDO

--- actividades/main.java
+++ actividades/main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class main {
     public static void main(String[] args) {
         JFrame ventana = new JFrame("Arbol de Pitagoras");
-        PythagorasTree panel = new PythagorasTree(3);
+        PythagorasTree panel = new PythagorasTree(0);
         ventana.add(panel);
         ventana.setSize(800,800);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
