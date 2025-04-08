4316552bf49826711959d1eba575b7bbf791a40f
kevmoslin <kevinsmostacero@gmail.com>
2025-04-08 16:02:22 -0500

    arreglos en el sout

--- actividades/Hanoi.java
+++ actividades/Hanoi.java
@@ -7,7 +7,7 @@ public class Hanoi {
 
     public static void torresHanoi(int discos, int torre1, int torre2, int torre3){
         if (discos == 1) {
-            System.out.println("Mover disco de torre " + torre1 + "a torre " + torre3);
+            System.out.println("Mover disco de torre " + torre1 + " a torre " + torre3);
         } else{
             torresHanoi(discos-1, torre1, torre3, torre2);
             System.out.println("Mover disco de torre " + torre1 + " a torre " + torre3);
