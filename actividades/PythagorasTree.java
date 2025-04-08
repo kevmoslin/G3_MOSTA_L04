bb11e2cfad56123d0a3ecc1452b97a6458553f8f
kevmoslin <kevinsmostacero@gmail.com>
2025-04-08 16:15:16 -0500

    atributo y constructor respectivo

--- actividades/PythagorasTree.java
+++ actividades/PythagorasTree.java
@@ -3,5 +3,13 @@ import javax.swing.JPanel;
 
 public class PythagorasTree extends JPanel{
     
+    private int profundidad;
+
+    public PythagorasTree(int profundidad){
+        this.profundidad = profundidad;
+        setPreferredSize(new Dimension(800, 800));
+    }
+
+    
     
 }
