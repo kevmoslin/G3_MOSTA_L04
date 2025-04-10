e9c49eee8fae958a6a8e0217344eb752254480d1
kevmoslin <kevinmostacerolinares@gmail.com>
2025-04-10 09:17:33 -0600

    arbol de 6 niveles

--- actividades/PythagorasTree.java
+++ actividades/PythagorasTree.java
@@ -35,11 +35,21 @@ public class PythagorasTree extends JPanel{
     }
 
     public static void main(String[] args) {
-        JFrame ventana = new JFrame("Arbol de Pitagoras");
-        PythagorasTree panel = new PythagorasTree(20);
+        
+        //Arbol de 3 niveles
+        JFrame ventana = new JFrame("Arbol de Pitagoras de 3 niveles");
+        PythagorasTree panel = new PythagorasTree(3);
         ventana.add(panel);
         ventana.setSize(800,800);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setVisible(true);
+
+        //arbol de 6 niveles
+        JFrame ventana1 = new JFrame("Arbol de Pitagoras de 6 niveles");
+        PythagorasTree panel1 = new PythagorasTree(6);
+        ventana1.add(panel1);
+        ventana1.setSize(800,800);
+        ventana1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana1.setVisible(true);
     }
 }
