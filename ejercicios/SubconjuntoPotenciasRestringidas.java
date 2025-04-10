6777f2fdbc6a5b4aa8988ee4ab61be5f9c5d5e92
kevmoslin <kevinmostacerolinares@gmail.com>
2025-04-10 11:23:14 -0600

    metodo puede alcazarObjetivo

--- ejercicios/SubconjuntoPotenciasRestringidas.java
+++ ejercicios/SubconjuntoPotenciasRestringidas.java
@@ -6,4 +6,33 @@ public class SubconjuntoPotenciasRestringidas {
     public static boolean esPotenciaDeDos(int n) {
         return n > 0 && (n & (n - 1)) == 0;
     }
+
+    public static boolean puedeAlcanzarObjetivo(int[] nums, int index, int objetivo, Set<Integer> obligatorios, Set<Integer> excluidos, Map<String, Boolean> memo) {
+        if (objetivo == 0) return true;
+        if (index == nums.length) return false;
+
+        String key = index + "-" + objetivo;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        int actual = nums[index];
+
+        if (excluidos.contains(index)) {
+            return puedeAlcanzarObjetivo(nums, index + 1, objetivo, obligatorios, excluidos, memo);
+        }
+        
+        if (obligatorios.contains(index)) {
+            return puedeAlcanzarObjetivo(nums, index + 1, objetivo, obligatorios, excluidos, memo);
+        }
+
+        boolean sinIncluir = puedeAlcanzarObjetivo(nums, index + 1, objetivo, obligatorios, excluidos, memo);
+
+        boolean incluir = false;
+        if (actual <= objetivo) {
+            incluir = puedeAlcanzarObjetivo(nums, index + 1, objetivo - actual, obligatorios, excluidos, memo);
+        }
+
+        boolean resultado = sinIncluir || incluir;
+        memo.put(key, resultado);
+        return resultado;
+    }
 }
