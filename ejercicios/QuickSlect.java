84ff39f1d02cb2ec645446d5d34843ba9f6fd315
kevmoslin <kevinmostacerolinares@gmail.com>
2025-04-10 08:31:19 -0600

    agregando ramdom

--- ejercicios/QuickSlect.java
+++ ejercicios/QuickSlect.java
@@ -1,7 +1,13 @@
 package ejercicios;
 
+import java.util.*;
+
 public class QuickSlect {
     private static int quickSelect(int[] arr, int left, int rigth, int k){
-        
+        if (left == rigth) {
+            return arr[left];
+        }
+
+        Random 
     }
 }
