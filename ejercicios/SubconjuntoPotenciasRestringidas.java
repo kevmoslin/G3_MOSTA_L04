fa6ba53c379de4512b5fcb5820291c2df4e3f9fa
kevmoslin <kevinmostacerolinares@gmail.com>
2025-04-10 11:24:27 -0600

    metodo verificar

--- ejercicios/SubconjuntoPotenciasRestringidas.java
+++ ejercicios/SubconjuntoPotenciasRestringidas.java
@@ -35,4 +35,8 @@ public class SubconjuntoPotenciasRestringidas {
         memo.put(key, resultado);
         return resultado;
     }
+
+    public static boolean verificar(int[] entrada){
+        
+    }
 }
