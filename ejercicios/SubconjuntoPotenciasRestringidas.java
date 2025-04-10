ccdc49aae78c66998e4fc7f761e3698a2d7b7a9f
kevmoslin <kevinmostacerolinares@gmail.com>
2025-04-10 11:22:48 -0600

    metodo potenciadedos

--- ejercicios/SubconjuntoPotenciasRestringidas.java
+++ ejercicios/SubconjuntoPotenciasRestringidas.java
@@ -3,5 +3,7 @@ import java.util.*;
 
 
 public class SubconjuntoPotenciasRestringidas {
-    
+    public static boolean esPotenciaDeDos(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
 }
